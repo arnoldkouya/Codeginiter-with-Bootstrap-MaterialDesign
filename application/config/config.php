179b0fb5111fdfa655dbae506e0d304d77ddd34a
Samuel Guebo <samuel@e-voir.net>
2015-05-13 09:43:27 +0000

    Making $config['base_url'] dynamic
    switch from static value of $config['base_url']  to dynamic

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://tuto.local/Codeginiter-with-Bootstrap-MaterialDesign/';
+//$config['base_url'] = 'http://tuto.local/Codeginiter-with-Bootstrap-MaterialDesign/';
+// switch from static value of $config['base_url']  to dynamic 
+$config['base_url'] =  ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ?  "https" : "http");
+$config['base_url'] .=  "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .=  str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
